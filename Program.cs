b4a5a002a844e0be3b4f7c85ae8bc4853ad81e4c
John Walters <john.walters.92117@gmail.com>
2023-01-03 20:55:22 -0800

    with fix for local dev 401 auth error problem.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using dotnetNgSite.Data;
 using dotnetNgSite.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
@@ -27,6 +28,15 @@ namespace dotnetNgSite
 
             builder.Services.AddAuthentication()
                 .AddIdentityServerJwt();
+            if (builder.Environment.IsDevelopment())
+            {
+                // to fix the 'WWW-Authenticate: Bearer error="invalid_token", error_description="The issuer 'https://example.com' is invalid" ' error that started occurring in out-of-the-box dot net 6 (& 7) with angular and authentication website projects, 
+                // add this line (get the port number from the project's .cproj file, <PropertyGroup>|<SpaProxyServerUrl>) :
+                builder.Services.Configure<JwtBearerOptions>("IdentityServerJwtBearer", o => o.Authority = "https://localhost:44491");
+                // thanks to Ogglas https://stackoverflow.com/users/3850405/ogglas for this
+                // https://stackoverflow.com/questions/60306175/bearer-error-invalid-token-error-description-the-issuer-is-invalid/66664653
+                // https://github.com/dotnet/core/blob/main/release-notes/6.0/known-issues.md#spa-template-issues-with-individual-authentication-when-running-in-development
+            }
 
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
